7a5a5400e922dc6d2bfd5907fc90c8a8d2e3f518
Matt Mould <etp404@users.noreply.github.com>
2020-11-04 13:05:46 +0000

    Example

--- PiP/ViewController.swift
+++ PiP/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    @IBAction func startPip(_ sender: Any) {
+        print("start pip")
+        pictureInPictureController.startPictureInPicture()
+    }
+    var player:AVPlayer!
+    var playerLayer:AVPlayerLayer!
+    var pictureInPictureController: AVPictureInPictureController!
+    
+    @IBOutlet var someView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        player = AVPlayer(url: URL(string: "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8")!)
+        playerLayer = AVPlayerLayer(player: player)
+        player.play()
+        someView.layer.addSublayer(playerLayer)
+        playerLayer.frame = self.someView.bounds //bounds of the view in which AVPlayer should be displayed
+        playerLayer.videoGravity = .resizeAspect
+        
+        pictureInPictureController = AVPictureInPictureController(playerLayer: playerLayer)
+
     }
 
 
